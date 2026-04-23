7779dfe8aa999a0f357367dfd6482a42f3087004
Annie Zhou <annie@CHD.local>
2026-04-22 17:49:12 -0700

    fix: remove double padding on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import Footer from "@/components/layout/Footer";
 
 export default function Home() {
   return (
-    <div className="px-[15%]">
+    <div>
       <HeroSection />
       <BookingSection />
       <ServicesSection />
